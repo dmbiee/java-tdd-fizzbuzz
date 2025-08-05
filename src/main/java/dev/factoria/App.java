c936e44dd44df8391a243cdf5251e4274b688fea
Sofia Belei <beleyhha@gmail.com>
2025-08-05 15:24:41 +0200

    init/init project

---
+++ src/main/java/dev/factoria/App.java
@@ -0,0 +1,10 @@
+package dev.factoria;
+
+public final class App {
+
+    public static void main(String[] args) {
+        for (byte i = 1; i < 101; i++) {
+            System.out.println(i);
+        }
+    }
+}
