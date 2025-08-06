bacc4cb4c6ddf09232ff6f680975111f6d58aaee
Sofia Belei <beleyhha@gmail.com>
2025-08-06 12:08:26 +0200

    resolve main exercice

--- src/main/java/dev/factoria/App.java
+++ src/main/java/dev/factoria/App.java
@@ -3,8 +3,10 @@ package dev.factoria;
 public final class App {
 
     public static void main(String[] args) {
+        System.out.println("\n\n\n");
+
         for (byte i = 1; i < 101; i++) {
-            System.out.println(i);
+            System.out.println("Current number is " + i);
         }
     }
 }
